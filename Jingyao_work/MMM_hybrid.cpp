cb34bdfe7a7c9f1e883a4d9acc91bf82025f6ea8
Jingyao711 <tangji19@msu.edu>
2024-03-25 23:21:30 -0400

    Code for part 3

--- Jingyao_work/MMM_hybrid.cpp
+++ Jingyao_work/MMM_hybrid.cpp
@@ -1,6 +1,7 @@
+#include <mpi.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 
 #define N 100 // Matrix size
 
@@ -16,10 +17,20 @@ void matrix_multiply(double A[N][N], double B[N][N], double C[N][N]) {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+    int rows_per_rank = N / size;
+    int start_row = rank * rows_per_rank;
+    int end_row = (rank + 1) * rows_per_rank;
     double A[N][N], B[N][N], C[N][N];
+
     // Initialize matrices A and B
-    for (int i = 0; i < N; i++) {
+    for (int i = start_row; i < end_row; i++) {
         for (int j = 0; j < N; j++) {
             A[i][j] = rand() % 10;
             B[i][j] = rand() % 10;
@@ -32,5 +43,13 @@ int main() {
 
     printf("Time taken: %f seconds\n", end_time - start_time);
 
+    double final_result[N][N];
+    MPI_Gather(C[start_row], rows_per_rank * N, MPI_DOUBLE,
+            final_result, rows_per_rank * N, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        // Print or save the final_result matrix
+    }
+    MPI_Finalize();
     return 0;
 }
